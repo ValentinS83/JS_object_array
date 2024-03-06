95cfe0546c9a87aa1b4bc79713310433eb6771bf
ValentinS83 <valentinstefan77@yahoo.com>
2024-03-06 19:53:23 +0200

    Add array shiift and push

---
+++ script.js
@@ -0,0 +1,32 @@
+const myProducts = ['Camasa', 'Pantaloni', 'Manusi'];
+console.log(`myProducts`);
+console.log(`first item in myProducts is ${myProducts[0]}`);
+console.log(`Lenght of myProducts array is ${myProducts.length}`);
+
+// adaugare de elemente
+const newItem ='Pantofi';
+myProducts.push(newItem);
+console.log(`myProducts after push:`, myProducts);
+
+myProducts.push(25);
+console.log(`myProducts after push:`, myProducts);
+
+// moidificarea unui element
+myProducts[0] = 'Tricou';
+console.log(`myProducts after changing first item:` , myProducts);
+
+// stergerea unui element
+myProducts.pop();
+console.log(`myProducts after pop-ing list:` , myProducts);
+
+// adaugarea unui element la inceputul array-ului
+myProducts.unshift('Camasa');
+console.log(`myProducts after unshift:` , myProducts);
+
+// stergerea unui element de la inceputul array-ului
+myProducts.shift();
+console.log(`myProducts after shift:` , myProducts);
+
+// indexof - gasirea indexului unui element din array
+const indexOfPantaloni = myProducts.indexOf('Pantaloni')
+console.log(`Elementul pantaloni se afla in lista la idexul ${indexOfPantaloni}`);
