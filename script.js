83b5da4b6bd0b3370aa6f5b605c83718c9442c61
ValentinS83 <valentinstefan77@yahoo.com>
2024-03-11 18:39:50 +0200

    Add example object

--- script.js
+++ script.js
@@ -33,13 +33,67 @@ console.log('myProducts after shift(): ', myProducts);
 const indexOfPantaloni = myProducts.indexOf('Pantaloni');
 console.log(`Elementul Pantaloni se afla in lista la indexul ${indexOfPantaloni}`);
 
+
 // subset al unui array
 const subsetOfMyProducts = myProducts.slice(1, 3);
-console.log(`subset from 1 to 3 is`, subsetOfMyProducts);
+console.log('subset from 1 to 3 is', subsetOfMyProducts);
 
 myProducts.splice(2, 1, 'Camasa');
-console.log(`After splice(2, 1, "Camasa"):`, myProducts);
+console.log('After splice(2, 1, "Camasa"): ', myProducts);
 
 // concatenare de array-uri
 const concatenatedArrays = myProducts.concat(subsetOfMyProducts);
 console.log('after concat(subsetOfMyProducts): ', concatenatedArrays);
+
+
+// Obiecte
+
+let person = {
+    name: "John",
+    surname: "Doe",
+    address: {
+        street: "Cetatii",
+        number: 21,
+        appartment: "b1",
+        block: "7b",
+        tronson: 1,
+        zipCode: "100203",
+        city: "Cluj-Napoca",
+        country: "Romania",
+        county: "Cluj"
+    },
+    age: 32
+};
+
+console.log("person: ", person);
+
+console.log(`nume persoana: ${person.name}`);
+
+console.log(`cod postal ${person.address.zipCode}`);
+
+// undefined property
+console.log(person.appartment);
+
+// brackets notation
+console.log(`person surname: ${person["surname"]}`);
+
+// modificare valori in obiect
+person.name = "Ion";
+console.log('name: ', person.name);
+
+//comparare obiecte
+const obj1 = {a: 10, b: true};
+const obj2 = {a: 10, b: true};
+const obj3 = obj1;
+
+console.log(`obj1 === obj2 => ${obj1 === obj2}`);
+console.log(`obj1 === obj3 => ${obj1 === obj3}`);
+
+
+// comparare dupa proprietati
+const areEqual = obj1.a === obj2.a && obj2.b === obj3.b;
+console.log('areEqual: ', areEqual);
+
+// stergere proprietate obiect
+delete person.age;
+console.log(`person: `, person);
